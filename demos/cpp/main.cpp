f7be90b988a02669d7c73fe2f0f152b4ae0197d2
201840186-Zhu Zhicheng <1249194828@qq.com>
2025-03-15 21:24:41 +0800

    初步实现了面向对象的建模，但还有很多细节需要完善

--- demos/cpp/main.cpp
+++ demos/cpp/main.cpp
@@ -2,38 +2,22 @@
 #include <cassert>
 #include <cstdlib>
 
-#define MAX_DISK_NUM (10 + 1)
-#define MAX_DISK_SIZE (16384 + 1)
-#define MAX_REQUEST_NUM (30000000 + 1)
-#define MAX_OBJECT_NUM (100000 + 1)
-#define REP_NUM (3)
-#define FRE_PER_SLICING (1800)
-#define EXTRA_TIME (105)
+#include "Constant.hpp"
+#include "Disk.hpp"
+#include "Object.hpp"
+#include "RequestScheduler.hpp"
+#include "TagManage.hpp"
+#include "StorageController.hpp"
 
-typedef struct Request_ {
-    int object_id;
-    int prev_id;
-    bool is_done;
-} Request;
-
-typedef struct Object_ {
-    int replica[REP_NUM + 1];
-    int* unit[REP_NUM + 1];
-    int size;
-    int last_request_point;
-    bool is_delete;
-} Object;
-
-Request request[MAX_REQUEST_NUM];
-Object object[MAX_OBJECT_NUM];
-
-
-
-
+/*
+T：代表本次数据有 𝑇+105个时间片，后续输入第二阶段将循环交互 𝑇+105次。 
+M：代表对象标签数。对象标签编号为 1 ~ 𝑀。
+N：代表存储系统中硬盘的个数，硬盘编号为 1 ~ 𝑁。
+V：代表存储系统中每个硬盘的存储单元个数。存储单元编号为1 ~ 𝑉。
+G：代表每个磁头每个时间片最多消耗的令牌数。 
+*/
 
 int T, M, N, V, G;
-int disk[MAX_DISK_NUM][MAX_DISK_SIZE];
-int disk_point[MAX_DISK_NUM];
 
 void timestamp_action()
 {
@@ -44,202 +28,73 @@ void timestamp_action()
     fflush(stdout);
 }
 
-void do_object_delete(const int* object_unit, int* disk_unit, int size)
-{
-    for (int i = 1; i <= size; i++) {
-        disk_unit[object_unit[i]] = 0;
-    }
-}
+// 预处理数据存储
+vector<vector<int>> fre_del, fre_write, fre_read;
 
-void delete_action()
-{
-    int n_delete;
-    int abort_num = 0;
-    static int _id[MAX_OBJECT_NUM];
-
-    scanf("%d", &n_delete);
-    for (int i = 1; i <= n_delete; i++) {
-        scanf("%d", &_id[i]);
-    }
-
-    for (int i = 1; i <= n_delete; i++) {
-        int id = _id[i];
-        int current_id = object[id].last_request_point;
-        while (current_id != 0) {
-            if (request[current_id].is_done == false) {
-                abort_num++;
-            }
-            current_id = request[current_id].prev_id;
-        }
-    }
-
-    printf("%d\n", abort_num);
-    for (int i = 1; i <= n_delete; i++) {
-        int id = _id[i];
-        int current_id = object[id].last_request_point;
-        while (current_id != 0) {
-            if (request[current_id].is_done == false) {
-                printf("%d\n", current_id);
-            }
-            current_id = request[current_id].prev_id;
-        }
-        for (int j = 1; j <= REP_NUM; j++) {
-            do_object_delete(object[id].unit[j], disk[object[id].replica[j]], object[id].size);
-        }
-        object[id].is_delete = true;
-    }
-
-    fflush(stdout);
-}
-
-void do_object_write(int* object_unit, int* disk_unit, int size, int object_id)
-{
-    int current_write_point = 0;
-    for (int i = 1; i <= V; i++) {
-        if (disk_unit[i] == 0) {
-            disk_unit[i] = object_id;
-            object_unit[++current_write_point] = i;
-            if (current_write_point == size) {
-                break;
-            }
-        }
-    }
-
-    assert(current_write_point == size);
-}
-
-void write_action()
-{
-    int n_write;
-    scanf("%d", &n_write);
-    for (int i = 1; i <= n_write; i++) {
-        int id, size;
-        scanf("%d%d%*d", &id, &size);
-        object[id].last_request_point = 0;
-        for (int j = 1; j <= REP_NUM; j++) {
-            object[id].replica[j] = (id + j) % N + 1;
-            object[id].unit[j] = static_cast<int*>(malloc(sizeof(int) * (size + 1)));
-            object[id].size = size;
-            object[id].is_delete = false;
-            do_object_write(object[id].unit[j], disk[object[id].replica[j]], size, id);
-        }
-
-        printf("%d\n", id);
-        for (int j = 1; j <= REP_NUM; j++) {
-            printf("%d", object[id].replica[j]);
-            for (int k = 1; k <= size; k++) {
-                printf(" %d", object[id].unit[j][k]);
-            }
-            printf("\n");
-        }
-    }
-
-    fflush(stdout);
-}
-
-void read_action()
-{
-    int n_read;
-    int request_id, object_id;
-    scanf("%d", &n_read);
-    for (int i = 1; i <= n_read; i++) {
-        scanf("%d%d", &request_id, &object_id);
-        request[request_id].object_id = object_id;
-        request[request_id].prev_id = object[object_id].last_request_point;
-        object[object_id].last_request_point = request_id;
-        request[request_id].is_done = false;
-    }
-
-    static int current_request = 0;
-    static int current_phase = 0;
-    if (!current_request && n_read > 0) {
-        current_request = request_id;
-    }
-    if (!current_request) {
-        for (int i = 1; i <= N; i++) {
-            printf("#\n");
-        }
-        printf("0\n");
-    } else {
-        current_phase++;
-        object_id = request[current_request].object_id;
-        for (int i = 1; i <= N; i++) {
-            if (i == object[object_id].replica[1]) {
-                if (current_phase % 2 == 1) {
-                    printf("j %d\n", object[object_id].unit[1][current_phase / 2 + 1]);
-                } else {
-                    printf("r#\n");
-                }
-            } else {
-                printf("#\n");
-            }
-        }
-
-        if (current_phase == object[object_id].size * 2) {
-            if (object[object_id].is_delete) {
-                printf("0\n");
-            } else {
-                printf("1\n%d\n", current_request);
-                request[current_request].is_done = true;
-            }
-            current_request = 0;
-            current_phase = 0;
-        } else {
-            printf("0\n");
-        }
-    }
-
-    fflush(stdout);
-}
-
-void clean()
-{
-    for (auto& obj : object) {
-        for (int i = 1; i <= REP_NUM; i++) {
-            if (obj.unit[i] == nullptr)
-                continue;
-            free(obj.unit[i]);
-            obj.unit[i] = nullptr;
-        }
-    }
-}
 
 int main()
 {
     scanf("%d%d%d%d%d", &T, &M, &N, &V, &G);
+    StorageController controller(N, V);
 
-    for (int i = 1; i <= M; i++) {
-        for (int j = 1; j <= (T - 1) / FRE_PER_SLICING + 1; j++) {
-            scanf("%*d");
+     // 预处理数据加载
+     auto load_fre = [&](vector<vector<int>>& dest) {
+        dest.resize(M + 1);
+        for(int i = 1; i <= M; i ++) {
+            int slices = (T + EXTRA_TIME) / FRE_PER_SLICING + 1;
+            dest[i].resize(slices + 1);
+            for(int j = 1; j <= slices; j ++)
+                scanf("%d", &dest[i][j]);
         }
-    }
-
-    for (int i = 1; i <= M; i++) {
-        for (int j = 1; j <= (T - 1) / FRE_PER_SLICING + 1; j++) {
-            scanf("%*d");
-        }
-    }
-
-    for (int i = 1; i <= M; i++) {
-        for (int j = 1; j <= (T - 1) / FRE_PER_SLICING + 1; j++) {
-            scanf("%*d");
-        }
-    }
+    };
+    
+    load_fre(fre_del);
+    load_fre(fre_write);
+    load_fre(fre_read);
 
     printf("OK\n");
     fflush(stdout);
 
-    for (int i = 1; i <= N; i++) {
-        disk_point[i] = 1;
-    }
 
-    for (int t = 1; t <= T + EXTRA_TIME; t++) {
+    for(int t = 1; t <= T + EXTRA_TIME; t ++) {
+        // 处理时间片对齐
         timestamp_action();
-        delete_action();
-        write_action();
-        read_action();
+        
+        // 处理删除事件
+        int n_delete;
+        scanf("%d", &n_delete);
+        // 处理删除逻辑 To do
+        vector<int> deleted;
+        while(n_delete --) {
+            int obj_id;
+            scanf("%d", &obj_id);
+            deleted.push_back(obj_id);
+        }
+
+        
+        // 处理写入请求
+        // To do
+        int n_write;
+        scanf("%d", &n_write);
+        while(n_write --) {
+            int id, size, tag;
+            scanf("%d %d %d", &id, &size, &tag);
+            controller.process_write(id, size, tag);
+        }
+        
+        // 处理读取请求
+        // To do
+        int n_read;
+        scanf("%d", &n_read);
+        while(n_read --) {
+            int req_id, obj_id;
+            scanf("%d %d", &req_id, &obj_id);
+            controller.process_read(req_id, obj_id);
+        }
+        
+        // 推进时间
+        controller.tick(G);
     }
-    clean();
 
     return 0;
 }
